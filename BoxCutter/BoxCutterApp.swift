092e2f13a83fdd709fb09375f314892eaff30c3e
hw4n <hw4n@icloud.com>
2026-02-20 16:14:10 +0900

    Initial Commit

---
+++ BoxCutter/BoxCutterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxCutterApp.swift
+//  BoxCutter
+//
+//  Created by hwx.nnn on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct BoxCutterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
